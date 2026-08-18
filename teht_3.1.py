7176f1e0e1ee0b2aaeeba58afe8ab41f4061834f
Santeri8754 <santeki@metropolia.fi>
2026-08-18 14:56:21 +0300

    teht_3.1

---
+++ teht_3.1.py
@@ -0,0 +1,9 @@
+# Lisätään syöte
+kuhan_pituus = float(input("Anna kuhan pituus"))
+
+#Tarkistetaan onko kuha alle 37 senttimetriä pienenmpi
+if kuhan_pituus < 37:
+    #Lasku toimitus lasketaan kuinka paljon kuha on alamittainen
+    alamitta = 37 - kuhan_pituus
+    print(f"kuhasi on {alamitta} cm liian lyhyt!")
+    print("Laske kuha takaisin järveen")
