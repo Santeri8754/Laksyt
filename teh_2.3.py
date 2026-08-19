d56c2fc5469c2c26d849d4d21731720d4ebf2593
Santeri8754 <santeki@metropolia.fi>
2026-08-19 11:39:58 +0300

    hemoglobiiniarvo tehtävä

--- teh_2.3.py
+++ teh_2.3.py
@@ -5,5 +5,6 @@ korkeus = float(input("Anna suorakulmion korkeus:"))
 #Laskutoimitus, jossa lasketaan piiri ja pintalal suorakulmiolle.
 ala= kanta*korkeus
 piiri= 2* (kanta + korkeus)
+
 # tulostetaan tulos.
 print(f"Suorakulmion pinta-ala on {ala:.2f} ja piiri on {piiri:.2f}")
