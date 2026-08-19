1f9ee5dfe42c74020a68881f69b199e581fee60a
Santeri8754 <santeki@metropolia.fi>
2026-08-19 13:31:14 +0300

    Karkausvuosi tehtävä

---
+++ teh_3.4.py
@@ -0,0 +1,8 @@
+#Kysytään vuosilukua
+vuosiluku=int(input("Anna vuosiluku: "))
+
+
+if vuosiluku % 4 == 0 and vuosiluku % 100 != 0 or vuosiluku % 400 == 0:
+    print("Vuosiluku on karkausvuosi")
+else:
+    print("Vuosiluku ei ole karkausvuosi")
