b7b064da5bfab3e9dd972a2c10fbab034a6e1cd1
Santeri8754 <santeki@metropolia.fi>
2026-08-19 10:55:22 +0300

    Koodinumero tehtävä

---
+++ teh_2.6.py
@@ -0,0 +1,18 @@
+import random
+
+#Arpoo 3 lukua 0-9 numeroiden väliltä ja luo näistä kolmi numeroisen koodin.
+luku1=(random.randint(0,9))
+luku2 = random.randint(0,9)
+luku3 = random.randint(0,9)
+
+#Tulostaa koodin
+print(f" koodi on {luku1} {luku2} {luku3}")
+
+#Arpoo nelinumeroisen koodin tässä.
+luku4=(random.randint(1,6))
+luku5 = random.randint(1,6)
+luku6=random.randint(1,6)
+luku7=random.randint(1,6)
+
+# Tulostaa koodin
+print(f" koodi on {luku4} {luku5} {luku6} {luku7}")
