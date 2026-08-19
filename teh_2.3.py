052928de1ebf2f59d0118bcaa6c80867e4bf5709
Santeri8754 <santeki@metropolia.fi>
2026-08-19 10:25:32 +0300

    Suorakulmion piiri ja pinta-ala tehtävä

---
+++ teh_2.3.py
@@ -0,0 +1,9 @@
+#Kysytään suorakulmion kantaa ja korkeuta
+kanta=float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus:"))
+
+#Laskutoimitus, jossa lasketaan piiri ja pintalal suorakulmiolle.
+ala= kanta*korkeus
+piiri= 2* (kanta + korkeus)
+# tulostetaan tulos.
+print(f"Suorakulmion pinta-ala on {ala:.2f} ja piiri on {piiri:.2f}")
