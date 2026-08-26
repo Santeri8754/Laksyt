568f2274178edc970c6d14f9a6bd6f5ffa5ab548
Santeri8754 <santeki@metropolia.fi>
2026-08-26 14:04:14 +0300

    kirjautumistehtävä

---
+++ teh_4.5.py
@@ -0,0 +1,18 @@
+kayttis=input("Anna käyttäjätunnus: ")
+salasana=input("Anna salasana: ")
+laskuri=0
+while kayttis!="python" or salasana!="rules":
+    laskuri += 1
+    print("Päsy evätty")
+    print(f"olet arvannut jo kerran {laskuri}")
+    if laskuri >=5:
+        print("Pääsysi on lopullisesti evätty")
+        break
+    kayttis=input("Anna kayttis uudestaan: ")
+    salasana=input("Anna salasana uudestaan: ")
+
+
+
+if kayttis=="python" or salasana=="rules":
+        print("tervetuloa")
+
