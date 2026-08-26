8bade3633f167517955386e35a14216b4f8610b3
Santeri8754 <santeki@metropolia.fi>
2026-08-26 11:38:25 +0300

    tuuma tehtävä

---
+++ teh_4.2.py
@@ -0,0 +1,6 @@
+tuuma = float(input("Anna tuuma: "))
+negatiivinen = float(0)
+while tuuma >= negatiivinen:
+    tuuma_cm = float(tuuma*2.54)
+    print(f" Vastaus on {tuuma_cm}")
+    tuuma = float(input("Anna tuuma: "))
