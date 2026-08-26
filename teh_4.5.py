a5c6195f2d4b10755afe126b485446dd9de83003
Santeri8754 <santeki@metropolia.fi>
2026-08-26 14:05:09 +0300

    kirjautumistehtävä

--- teh_4.5.py
+++ teh_4.5.py
@@ -3,7 +3,7 @@ salasana=input("Anna salasana: ")
 laskuri=0
 while kayttis!="python" or salasana!="rules":
     laskuri += 1
-    print("Päsy evätty")
+    print("Pääsy evätty")
     print(f"olet arvannut jo kerran {laskuri}")
     if laskuri >=5:
         print("Pääsysi on lopullisesti evätty")
